bb3520457c9fed29ffd3fd1ffa49202d3688d422
Bensonate <bensonate98@gmail.com>
2025-02-19 13:02:09 -0800

    update

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import moment from "moment";
+import integrationSpecSettings from "./settings.js";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res)=>{
+  res.send("Hello Integration World");
+});
+
+//json url
+app.get("/integration.json", (req, res)=>{
+  return res.json(integrationSpecSettings);
+})
+
+const birthdayList = [
+  {name: "Benson", dateOfbirth: "1998-10-23"},
+  {name: "Mary", dateOfbirth: "2000-02-19"},
+  {name: "John", dateOfbirth: "2001-02-19"},
+  {name: "Benson", dateOfbirth: "1999-02-20"}
+];
+
+// Tick url
+app.post("/check-birthdays", (req, res)=>{
+  const today = moment().format("MM-DD");
+
+  const birthdaysToday = birthdayList.filter((person)=>{
+    const birthday = moment(person.dateOfbirth, "YYY-MM-DD").format("MM-DD");
+    return birthday === today;
+  })
+  
+  if(birthdaysToday.length > 0){
+    const message = birthdaysToday.map(person=>`Hurray!🎉 It's ${person.name}'s birthday. Happy birthday ${person.name}, May God bless and keep you.`)
+    return res.status(200).json({message});
+  }
+  return res.json({ messages: [] });
+})
+
+
+app.listen(PORT, ()=>{
+  console.log(`server running on port ${PORT}`);
+})
---
+++ src/settings.js
@@ -0,0 +1,60 @@
+const integrationSpecSettings = {
+  "data": {
+    "date": {
+      "created_at": "2025-02-19",
+      "updated_at": "2025-02-19S"
+    },
+    "descriptions": {
+      "app_description": "Birthday Reminder notifies your team about upcoming birthdays every morning.",
+      "app_logo": "https://img.freepik.com/free-psd/yellow-gift-with-golden-ribbon-icon-sign-symbol-3d-background-illustration_56104-2422.jpg",
+      "app_name": "Birthday Reminder",
+      "app_url": "https://tft7wd0n-3000.uks1.devtunnels.ms/",
+      "background_color": "#fff"
+    },
+    "integration_category": "Monitoring & Logging",
+    "integration_type": "interval",
+    "is_active": true,
+    "output": [
+      {
+        "label": "bensonate-channel-0",
+        "value": true
+      }
+    ],
+    "key_features": [
+      "Daily birthday reminders at a set time.",
+      "Customizable messages.",
+      "Keeps team engagement high."
+    ],
+    "permissions": {
+      "monitoring_user": {
+        "always_online": true,
+        "display_name": "Birthday Bot"
+      }
+    },
+    "settings": [
+      {
+        "label": "interval",
+        "type": "text",
+        "required": true,
+        "default": "0 8 * * *",
+        "required": true
+      },
+      // {
+      //   "label": "Birthday list",
+      //   "type": "text",
+      //   "description": "Enter the API URL or database endpoint where birthdays are stored.",
+      //   "default": "//////",
+      //   "required": true
+      // },
+      // {
+      //   "label": "Message Message",
+      //   "description": "Custom message format (use {name} for dynamic names).",
+      //   "required": true,
+      //   "default": "🎉 Happy Birthday, {name}! 🎂"
+      // }
+    ],
+    "tick_url": "https://tft7wd0n-3000.uks1.devtunnels.ms/api/v1/check-birthdays",
+  }
+}
+
+export default integrationSpecSettings;
