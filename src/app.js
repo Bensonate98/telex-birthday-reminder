ba914d979368e53b16a1c855fdb0f6ab18b22b85
Bensonate <bensonate98@gmail.com>
2025-02-20 06:33:18 -0800

    update

--- src/app.js
+++ src/app.js
@@ -22,6 +22,8 @@ const birthdayList = [
 
 // Tick url
 app.post("/check-birthdays", (req, res)=>{
+  const {channel_id, settings } = req.body;
+  console.log(channel_id, settings);
   const today = moment().format("MM-DD");
 
   const birthdaysToday = birthdayList.filter((person)=>{
