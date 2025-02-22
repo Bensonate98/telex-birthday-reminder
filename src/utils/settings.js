e60e589132f88ba0a7d442c3fa49e3fa2833ad16
Bensonate <bensonate98@gmail.com>
2025-02-22 04:07:33 -0800

    update

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -5,7 +5,7 @@ const integrationSpecSettings = {
       "updated_at": "2025-02-19"
     },
     "descriptions": {
-      "app_description": "Birthday Reminder notifies your team about upcoming birthdays every morning.",
+      "app_description": "Birthday Reminder notifies your team about members birthday by sending a birthday wish (message) to the channel",
       "app_logo": "https://img.freepik.com/free-psd/yellow-gift-with-golden-ribbon-icon-sign-symbol-3d-background-illustration_56104-2422.jpg",
       "app_name": "Birthday Reminder",
       "app_url": "https://telex-birthday-reminder.vercel.app",
@@ -16,15 +16,9 @@ const integrationSpecSettings = {
     "target_url": "",
     "is_active": true,
     "author": "Benson Christopher Emuzo",
-    "output": [
-      {
-        "label": "bensonate-channel-0",
-        "value": true
-      }
-    ],
     "key_features": [
       "Birthday messages for team members",
-      "Customizable birthday messages.",
+      "Customizable time for notification",
       "Keeps team engagement high."
     ],
     "settings": [
