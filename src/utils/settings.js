da3d18233e8e4d9a9222cde45a54bc03a0bccd6f
Benson Christopher <125814252+Bensonate98@users.noreply.github.com>
2025-02-23 11:48:15 +0100

    Update settings.js

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -5,7 +5,7 @@
       "updated_at": "2025-02-19"
     },
     "descriptions": {
-      "app_description": "Birthday Reminder notifies your team about members birthday by sending a birthday wish (message) to the channel",
+      "app_description": "Birthday Reminder notifies your team about members birthday by sending a birthday wish (message) to the channel at a set time. Default is 6am",
       "app_logo": "https://img.freepik.com/free-psd/yellow-gift-with-golden-ribbon-icon-sign-symbol-3d-background-illustration_56104-2422.jpg",
       "app_name": "Birthday Reminder",
       "app_url": "https://telex-birthday-reminder.vercel.app",
@@ -17,9 +17,9 @@
     "is_active": true,
     "author": "Benson Christopher Emuzo",
     "key_features": [
-      "Birthday messages for team members",
-      "Customizable time for notification",
-      "Keeps team engagement high."
+      "*How to use Birthday Reminder*",
+      "Ensure your team members fill in their names and birthdays using this link: https://birthday-saver.vercel.app/",
+      "Activate Birthday Reminder on your channel and configure the set interval you prefer. Default is 6am"
     ],
     "settings": [
       {
